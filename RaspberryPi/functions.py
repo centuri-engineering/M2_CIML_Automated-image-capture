d15e14a84f055fd055d6b6dbb8f46c6bad048958
Mathias Lechelon <mathias.lechelon@univ-amu.fr>
2020-12-02 17:26:50 +0100

    final modif

--- RaspberryPi/functions.py
+++ RaspberryPi/functions.py
@@ -7,18 +7,18 @@ import serial
 import time
 import numpy as np
 from picamera import PiCamera
-from config import *
+from config import box, box_array, info, cam_set, ser_set
+from itertools import product
 
 # VARIABLES:
 # Time when loops start:
 starting_loops = np.arange(0, info["total_duration"] + 1, info["elapse_time"])
 
-nb_well = box["nb_well_along_X"] * box["nb_well_along_Y"]
-nb_box = box_array["nb_box_along_X"] * box_array["nb_box_along_Y"]
 
 # FUNCTIONS
 # Function 1: creating a zigzag pattern to place the camera above each well, and returning X and Y coordinates
 def well_scanning_zigzag(box):
+    nb_well = box["nb_well_along_X"] * box["nb_well_along_Y"]
     # Distance for a single move in X = Y:
     dish_move = box["dist_inter_well"] + box["diam_well"]
     # Y coordinates:
@@ -42,6 +42,7 @@ def well_scanning_zigzag(box):
 
 # Function 2 : creating a zigzag pattern to place the camera above each 1st well of each box, and returning X and Y coordinates
 def box_scanning_zigzag(box_array):
+    nb_box = box_array["nb_box_along_X"] * box_array["nb_box_along_Y"]
     # Distance for a single move:
     box_move_along_X = box["size_along_X"] + box_array["dist_inter_box_X"]
     box_move_along_Y = box["size_along_Y"] + box_array["dist_inter_box_Y"]
@@ -69,6 +70,8 @@ def box_scanning_zigzag(box_array):
 
 
 def camera_control(well_coord, nb_well, box_coord, nb_box, starting_loops):
+    nb_well = box["nb_well_along_X"] * box["nb_well_along_Y"]
+    nb_box = box_array["nb_box_along_X"] * box_array["nb_box_along_Y"]
     # Use a control manager to open serial port:
     with serial.Serial(ser_set["board_path"], ser_set["baudrate"]) as s:
         # Wake up grbl : waiting 2 seconds is enough to wake it up. Some also use "s.write(b"\r\n\r\n")" but it is not needed.
@@ -84,28 +87,16 @@ def camera_control(well_coord, nb_well, box_coord, nb_box, starting_loops):
         camera.start_preview(fullscreen=False, window=(100, 20, 640, 480))
         start = time.time()
         for it0, start_loop in enumerate(starting_loops):
-            # if it0 != 0:
             while it0 != 0 and (int(time.time()) - int(start)) != int(start_loop):
                 pass
-            for it1 in range(nb_box):
-                for it2 in range(nb_well):
-                    s.write(
-                        b"G0 X"
-                        + str(well_coord[0, it2] + box_coord[1, it1]).encode()
-                        + b" Y"
-                        + str(well_coord[0, it2] + box_coord[1, it1]).encode()
-                        + b"\n"
-                    )
-                    camera.capture(
-                        "images/image"
-                        + str(it0 + 1)
-                        + "_box"
-                        + str(it1 + 1)
-                        + "_well"
-                        + str(it2 + 1)
-                        + ".jpg"
-                    )
-                    time.sleep(cam_set["delay_for_picture"])
+                for it1, it2 in product(range(nb_box), range(nb_well)):
+                    x_mv = str(well_coord[0, it2] + box_coord[0, it1]).encode()
+                    y_mv = str(well_coord[1, it2] + box_coord[1, it1]).encode()
+                    s.write(b"GO X" + x_mv + b" Y" + y_mv + b"\n")
 
-        camera.stop_preview()
-        s.close()
+                    im_path = (
+                        f"images/image{it0+1:04d}_box{it1+1:04d}_well{it2+1:04d}.jpg"
+                    )
+                    camera.capture(im_path)
+                    time.sleep(cam_set["delay_for_picture"])
+    camera.stop_preview()
